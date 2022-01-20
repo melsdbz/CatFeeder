c7f0f0e41860fa82a50d77443b689d9b802757a2
melsdbz <melsdbz@gmail.com>
2022-01-20 00:35:13 -0500

    Added Twitterbot to CatFeeder

--- CatFeeder.py
+++ CatFeeder.py
@@ -5,6 +5,7 @@ import time
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+import twitterBot
 
 #dotenv for environmental variables
 load_dotenv()
@@ -40,7 +41,7 @@ def feedByGmail():
             time.sleep(4)
             msg = gmailWrapper.createMsg(date_name, SMTP_user, email_addr, filename)
             gmailWrapper.sendMsg(msg)
-            tweetplz(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, email_addr, date_name, filename)
+            twitterBot.tweetplz(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, email_addr, date_name, filename)
         except:
             print("Couldn't feed Gigi his big meal :/")           
     if(len(snackIds) > 0):
