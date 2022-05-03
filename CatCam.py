11b1985cccef6f1d2d8966f0f9d3d87dcb47d3cd
root <root@raspberrypi>
2022-05-02 22:40:23 -0400

    add filters

--- CatCam.py
+++ CatCam.py
@@ -1,11 +1,14 @@
 import picamera
 import time
+import random
 
 def takePic(filename):
 
     cam = picamera.PiCamera()
     cam.resolution = (1600, 1200)
     cam.start_preview
+    effects = list(cam.IMAGE_EFFECTS.keys())
+    cam.image_effect = effects[random.randint(0,21)]
     time.sleep(3)
     cam.capture(filename)
     cam.stop_preview
